4e15f9faa348f3f701f134535f00b769ce4d39d2
farouk mohamed <faroukalghannam@gmail.com>
2025-03-08 09:07:35 +0200

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: "full" },
+    {path : 'home' ,component :HomeComponent},
+    {path:"**",component:NotFoundComponent}
+
+ 
+];
